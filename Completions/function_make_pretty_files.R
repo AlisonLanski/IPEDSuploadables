1e228bddb9235672932a1d5f40bf34fadf22d03c
Shiloh <shilohbradley@yahoo.com>
2020-02-18 11:27:04 -0800

    made pretty function in tidy

--- Completions/function_make_pretty_files.R
+++ Completions/function_make_pretty_files.R
@@ -3,21 +3,13 @@
 ## uploaded IPEDS data.
 #####
 
-library(stringr)
-
 make_pretty_files <- function(df = partA) {
   
-  dt <- data.frame()
-  for(i in 1:nrow(df)) {
-    for(j in 1:ncol(df)) {
-      dt[i,j] <- str_replace_all(df[i,j], "^[:upper:]+[=]*", "")
-    }
-  }
-  names(dt) <- names(df)
-  
-  return(dt)
+  df %>%
+    purrr::map_df(~stringr::str_replace_all(., "^[:upper:]+[=]*", ""))
 }
 
-# df <- make_pretty_files(partA)
+## Example
+df <- make_pretty_files(partA)
 
 
