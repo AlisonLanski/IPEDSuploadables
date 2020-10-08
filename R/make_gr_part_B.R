ee76905a57e63d51fa5c93d555fce66a9ce29a39
Shiloh Bradley <33234302+shilohbradley@users.noreply.github.com>
2020-10-08 14:56:09 -0700

    initial commit

---
+++ R/make_gr_part_B.R
@@ -0,0 +1,45 @@
+#' Make Graduation Rates Part B
+#'
+#' @param df A dataframe of student/degree information
+#' @param output A string (\code{"part"}, \code{"full"}, or \code{"both"})
+#' @param format A string (\code{"uploadable"}, \code{"readable"}, or \code{"both"})
+#'
+#' @importFrom rlang .data
+#' @importFrom magrittr "%>%"
+#' @importFrom dplyr select group_by summarize ungroup bind_rows arrange transmute n
+#' @importFrom utils write.table
+#'
+#' @return A text file
+#' @export
+#'
+
+
+make_gr_part_B <- function(df, output = "part", format = "both") {
+
+
+  #produce the uploadable format
+  partB <- df %>%
+    #aggregate the full data
+    dplyr::group_by(.data$Unitid, .data$Section, .data$Line, .data$RaceEthnicity, .data$Sex) %>%
+    dplyr::summarize(Count = dplyr::n()) %>%
+    dplyr::ungroup() %>%
+    #sort for easy viewing
+    dplyr::arrange(.data$Section, .data$Line, .data$RaceEthnicity, .data$Sex) %>%
+    #format for upload
+    dplyr::transmute(UNITID = paste0("UNITID=", .data$Unitid),
+                     SURVSECT = "SURVSECT=GR1",
+                     PART = "PART=B",
+                     SECTION = paste0("SECTION=", .data$Section),
+                     LINE = paste0("LINE=", .data$Line),
+                     RACE = paste0("RACE=", .data$RaceEthnicity),
+                     SEX = paste0("SEX=", .data$Sex),
+                     COUNT = paste0("COUNT=", .data$Count)
+                     )
+
+  #create the txt file
+  write_report(df = partB,
+               component = "GradRates",
+               part = "PartB",
+               output = output,
+               format = format)
+}
---
+++ R/make_gr_part_C.R
@@ -0,0 +1,40 @@
+#' Make Graduation Rates Part C
+#'
+#' @param df A dataframe of student/degree information
+#' @param output A string (\code{"part"}, \code{"full"}, or \code{"both"})
+#' @param format A string (\code{"uploadable"}, \code{"readable"}, or \code{"both"})
+#'
+#' @importFrom rlang .data
+#' @importFrom dplyr select group_by summarize ungroup arrange transmute n
+#' @importFrom utils write.table
+#'
+#' @return A text file
+#' @export
+#'
+
+make_gr_part_C <- function(df, output = "part", format = "both") {
+
+  partC <- df %>%
+    dplyr::select(.data$Unitid, .data$Section, .data$Line, .data$PellGrant, .data$DirectLoan) %>%
+    #deduplicate
+    unique() %>%
+    #aggregate and count
+    dplyr::group_by(.data$Unitid, .data$Section, .data$Line) %>%
+    dplyr::summarize(Count = dplyr::n()) %>%
+    dplyr::ungroup() %>%
+    #format for upload
+    dplyr::transmute(UNITID = paste0("UNITID=", .data$Unitid),
+                     SURVSECT = "SURVSECT=GR1",
+                     PART = "PART=C",
+                     SECTION = paste0("SECTION=", .data$Section),
+                     LINE = paste0("LINE=", .data$Line),
+                     PELLGRANT_RCPT = paste0("SEX=", .data$PellGrant),
+                     DIRECTLOAN_RCPT = paste0("DIRECTLOAN_RCPT=", .data$DirectLoan)
+                     )
+
+  write_report(df = partC,
+               component = "GradRates",
+               part = "PartC",
+               output = output,
+               format = format)
+}
