ca8b89e7a33c3645415bef701ba558b25a19f8e4
Shiloh Bradley <33234302+shilohbradley@users.noreply.github.com>
2021-01-08 11:41:34 -0800

    devtools::check()

--- R/create_dummy_data_gr.R
+++ R/create_dummy_data_gr.R
@@ -1,5 +1,7 @@
 #' Create dummy data for testing the Grad Rates functions
 #' @description Creates dummy data for testing the Grad Rates functions
+#' @param n Number of rows of data to synthesize
+#' @param seed Seed number to supply to \code{"set.seed()"} to replicate of results
 #' @return a dataframe ready for the rest of the Grad Rates functions
 #' @export
 #'
