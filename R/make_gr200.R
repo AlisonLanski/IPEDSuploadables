da70c721b578a4fa201e25dffd2169bee762ab6b
Shiloh Bradley <shilohfling@gmail.com>
2021-11-19 11:30:43 -0800

    gr200 colnames toupper

--- R/make_gr200.R
+++ R/make_gr200.R
@@ -8,6 +8,7 @@
 #' @importFrom magrittr "%>%"
 #' @importFrom dplyr group_by summarize ungroup transmute
 #' @importFrom utils write.table
+#' @importFrom stringr str_to_upper
 #'
 #' @return A text file
 #' @export
@@ -16,19 +17,21 @@
 
 make_gr200 <- function(df, output = "part", format = "both") {
 
+  colnames(df) <- stringr::str_to_upper(colnames(df))
+
   gr200 <- df %>%
-    dplyr::group_by(.data$Unitid) %>%
-    dplyr::summarize(Exclusions = sum(.data$IsExclusion),
-                    Comp = sum(.data$IsComp),
-                    StillEnrolled = sum(.data$IsStillEnrolled)) %>%
+    dplyr::group_by(.data$UNITID) %>%
+    dplyr::summarize(EXCLUSIONS = sum(.data$ISEXCLUSION),
+                     COMP = sum(.data$ISCOMP),
+                     STILLENROLLED = sum(.data$ISSTILLENROLLED)) %>%
     dplyr::ungroup() %>%
     #format for upload
-    dplyr::transmute(UNITID = paste0("UNITID=", .data$Unitid),
+    dplyr::transmute(UNITID = paste0("UNITID=", .data$UNITID),
                      SURVSECT = "SURVSECT=G21",
                      PART = "PART=A",
-                     ADEXCL = paste0("ADEXCL=", .data$Exclusions),
-                     `COMPY7-8` = paste0("COMPY7-8=", .data$Comp),
-                     STILLENROLLED = paste0("STILLENROLLED=", .data$StillEnrolled),
+                     ADEXCL = paste0("ADEXCL=", .data$EXCLUSIONS),
+                     `COMPY7-8` = paste0("COMPY7-8=", .data$COMP),
+                     STILLENROLLED = paste0("STILLENROLLED=", .data$STILLENROLLED),
                      )
 
   #create the txt file
