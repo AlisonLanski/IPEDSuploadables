2bd88b0f8767c6a5df6fc5d8aff4d03d993ea169
Alison Lanski <alanski@nd.edu>
2022-10-03 09:35:24 -0400

    Reusable code for when IPEDS changes force changes to starting data files or file structures

---
+++ DataUpdateCode.R
@@ -0,0 +1,27 @@
+### Reminder bits of code when the package needs serious structural updates
+
+### Rewrite dummy data and stored part outputs
+
+####################
+##Completions
+######
+
+# #Data
+# com_students <- create_dummy_data_com("student")
+# use_data(com_students, overwrite = T)
+#
+# com_cips <- create_dummy_data_com("cip")
+# use_data(com_cips, overwrite = T)
+#
+# #Vignettes: see vignette scripts
+#
+# #Output
+# part_outputs$com_partA <- make_com_part_A(prep_com_data_frame(com_students),
+#                                           prep_com_data_frame(com_cips))
+# part_outputs$com_partB <- make_com_part_B(prep_com_data_frame(com_students),
+#                                           prep_com_data_frame(com_cips))
+# part_outputs$com_partC <- make_com_part_C(prep_com_data_frame(com_students))
+# part_outputs$com_partD <- make_com_part_D(prep_com_data_frame(com_students),
+#                                           prep_com_data_frame(com_cips))
+# part_outputs$com_partE <- make_com_part_E(prep_com_data_frame(com_students))
+# use_data(part_outputs, overwrite = T)
