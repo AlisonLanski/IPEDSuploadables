a4fe00c57c31695ce4d3b5b005829d0ffcefe1ad
Alison Lanski <alanski@nd.edu>
2022-12-07 08:36:11 -0500

    startup message update for next release

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage("This version of IPEDSuploadables has been updated for \nFALL collection of the 2022-2023 reporting cycle.\nWinter and Spring updates coming soon.\nIf you are in a later cycle or want to capture any bug fixes,\nplease update the package.")
+  packageStartupMessage("This version of IPEDSuploadables has been updated for \nFALL & WINTER collection of the 2022-2023 reporting cycle.\nSpring updates coming soon.\nIf you are in a later cycle or want to capture any bug fixes,\nplease update the package.")
 }
