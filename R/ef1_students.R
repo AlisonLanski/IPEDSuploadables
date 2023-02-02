5dbbb59499fa451a2571a422ca0a23f9f15796b4
Alison Lanski <alanski@ND.EDU>
2023-02-02 17:14:27 -0500

    updating dummy data and spec for EF1 to match GenderDetail IPEDS update

--- R/ef1_students.R
+++ R/ef1_students.R
@@ -1,8 +1,9 @@
 #' Dummy student data for Fall Enrollment scripts
-#' @format A data frame with 100 rows and 16 columns
-#' @description Using the default number of students and random seed, this data
+#' @format A data frame with 100 rows and 25 columns
+#' @description Using the default number of students, this data
 #'   provides a population that touches most available categories of student
 #'   reporting. Some columns use only a selection of possible values to reduce
 #'   complexity.
+#' @note To recreate the saved dataframe exactly, use seed 1234 with 100 students.
 
 "ef1_students"
