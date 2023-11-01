755dd085b9769ef46a6018fa070130aa09392d71
Alison Lanski <alanski@nd.edu>
2023-11-01 09:33:50 -0400

    maybe this will eliminate startup  messages

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,11 @@
 .onAttach <- function(libname, pkgname){
   packageStartupMessage("This version of IPEDSuploadables has been updated for \nthe 2023-2024 reporting cycle.\nIf you are in a later cycle or want to capture any bug fixes,\nplease update the package.")
 }
+
+.onLoad <- function(libname, pkgname) {
+  invisible(suppressPackageStartupMessages(
+    sapply(c("dplyr", "lubridate", "magrittr", "purrr",
+             "rlang", "stringr", "tidyr", "utils", "svDialogs"),
+           requireNamespace, quietly = TRUE)
+  ))
+}
