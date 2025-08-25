67c3e6314eea83ce981a5c6c85f3a947aa9a554d
Shiloh Fling <shilohfling@gmail.com>
2025-08-25 11:00:07 -0700

    Fall Enrollment report on binary sex instead of gender
    deprecating ggender and ugender

--- R/make_ef1_part_H.R
+++ R/make_ef1_part_H.R
@@ -1,12 +1,14 @@
-#' Make Fall Enrollment Part H (gender details)
+#' Make Fall Enrollment Part H (Sex Unknown)
 #'
 #' @param df A dataframe of student enrollment information
-#' @param ugender A boolean: TRUE means you are collecting and able to report
+#' @param ugender `r lifecycle::badge("deprecated")` A boolean: TRUE means you are collecting and able to report
 #'   "another gender" for undergraduate completers, even if you have no (or few)
-#'   such students. Set as FALSE if necessary
-#' @param ggender A boolean: TRUE means you are collecting and able to report
+#'   such students. Set as FALSE if necessary. **Starting in 2025-2026, this argument will be ignored by later
+#'   code.**
+#' @param ggender `r lifecycle::badge("deprecated")` A boolean: TRUE means you are collecting and able to report
 #'   "another gender" for graduate completers, even if you have no (or few) such
-#'   students. Set as FALSE if necessary
+#'   students. Set as FALSE if necessary. **Starting in 2025-2026, this argument will be ignored by later
+#'   code.**
 #'
 #' @importFrom rlang .data
 #' @importFrom dplyr select group_by summarize ungroup arrange transmute n
@@ -18,7 +20,23 @@
 #' @export
 #'
 
-make_ef1_part_H <- function(df, ugender, ggender) {
+make_ef1_part_H <- function(df, ugender = lifecycle::deprecated(), ggender = lifecycle::deprecated()) {
+
+  if (lifecycle::is_present(ugender)) {
+    lifecycle::deprecate_warn(
+      when = "2.11.0",
+      what = "make_ef1_part_H(ugender)",
+      details = "Detailed gender reporting is no longer used for this IPEDS survey. Argument may be removed in future versions."
+    )
+  }
+
+  if (lifecycle::is_present(ggender)) {
+    lifecycle::deprecate_warn(
+      when = "2.11.0",
+      what = "make_ef1_part_H(ggender)",
+      details = "Detailed gender reporting is no longer used for this IPEDS survey. Argument may be removed in future versions."
+    )
+  }
 
   colnames(df) <- stringr::str_to_upper(colnames(df))
 
@@ -26,8 +44,9 @@ make_ef1_part_H <- function(df, ugender, ggender) {
     dplyr::select("UNITID",
                   "STUDENTID",
                   "STUDENTLEVEL",
-                  "GENDERDETAIL"  #Binary = 1, 2;  Unknown = 3, Another = 4
+                  "SEX"  #Binary = 1, 2
     ) %>%
+    dplyr::filter(.data$SEX != 1 & .data$SEX != 2) %>%
     #break into UG and GR levels
     dplyr::mutate(UGPB = ifelse(.data$STUDENTLEVEL == 'Graduate', 'GR', 'UG')) %>%
     dplyr::select(-"STUDENTLEVEL") %>%
@@ -36,98 +55,25 @@ make_ef1_part_H <- function(df, ugender, ggender) {
     #aggregate and count
     dplyr::group_by(.data$UNITID,
                     .data$UGPB,
-                    .data$GENDERDETAIL) %>%
+                    .data$SEX) %>%
     dplyr::summarize(COUNT = dplyr::n()) %>%
     dplyr::ungroup() %>%
     #sort for easy viewing
-    dplyr::arrange(.data$UGPB, .data$GENDERDETAIL)
+    dplyr::arrange(.data$UGPB, .data$SEX)
 
   #set up the final DF
   partH <- data.frame(UNITID = unique(partH_counts$UNITID),
                       SURVSECT = "EF1",
                       PART = "H",
-                      EFGU01 = 0,  #UG detail reporting?
-                      EFGU011 = 0, #UG Unknown
-                      EFGU012 = 0, #UG Another
-                      EFGU02 = 0,  #GR detail reporting?
-                      EFGU021 = 0, #GR Unknown
-                      EFGU022 = 0) #GR Another
+                      EFSEXUG = partH_counts$COUNT[partH_counts$UGPB == "UG"],
+                      EFSEXG = partH_counts$COUNT[partH_counts$UGPB == "GR"])
 
-  #ugly way to get the right counts in each bit.
-  #I am sure there is a much nicer way to do it with a pivot
-  # and maybe with a dummy table for all values. I dunno. This works.
 
-  # #No UG in the data
-  if(!'UG' %in% partH_counts$UGPB){
-    partE$EFGU01 <- 2  #not reporting another
-    partE$EFGU012 <- -2  #not applicable
-
-    #  #UG in the data, another not being reported, unknown might exist
-  } else if(ugender == FALSE){
-    partH$EFGU01 <- 2
-    if(sum(partH_counts$UGPB == 'UG' &
-           partH_counts$GENDERDETAIL == 3) == 1){
-      partH$EFGU011 <- partH_counts$COUNT[partH_counts$UGPB == 'UG' &
-                                           partH_counts$GENDERDETAIL == 3]
-    }
-    partH$EFGU012 <- -2
-
-    # #UG in the data, another being reported, another/unknown might or might not exist
-  } else {
-    partH$EFGU01 <- 1
-
-    if(sum(partH_counts$UGPB == 'UG' &
-           partH_counts$GENDERDETAIL == 3) == 1){
-      partH$EFGU011 <- partH_counts$COUNT[partH_counts$UGPB == 'UG' &
-                                           partH_counts$GENDERDETAIL == 3]
-    }
-    if(sum(partH_counts$UGPB == 'UG' &
-           partH_counts$GENDERDETAIL == 4) == 1){
-      partH$EFGU012 <- partH_counts$COUNT[partH_counts$UGPB == 'UG' &
-                                           partH_counts$GENDERDETAIL == 4]
-    }
-    #BUT -- New in 2023 - mask if < 5 and set initial inquiry as "small N"
-    if(partH$EFGU012 < 5){
-      partH$EFGU012 <- -2
-      partH$EFGU01 <- 3
-    }
-  }
-
-  # #No GR in the data
-  if(!'GR' %in% partH_counts$UGPB){
-    partH$EFGU02 <- 2  #not reporting another
-    partH$EFGU022 <- -2  #not applicable
-
-    #  #GR in the data, another not being reported, unknown might exist
-  } else if(ggender == FALSE){
-    partH$EFGU02 <- 2
-    if(sum(partH_counts$UGPB == 'GR' &
-           partH_counts$GENDERDETAIL == 3) == 1){
-      partH$EFGU021 <- partH_counts$COUNT[partH_counts$UGPB == 'GR' &
-                                           partH_counts$GENDERDETAIL == 3]
-    }
-    partH$EFGU022 <- -2
-
-    # #GR in the data, another being reported, another/unknown might or might not exist
-  } else {
-    partH$EFGU02 <- 1
-
-    if(sum(partH_counts$UGPB == 'GR' &
-           partH_counts$GENDERDETAIL == 3) == 1){
-      partH$EFGU021 <- partH_counts$COUNT[partH_counts$UGPB == 'GR' &
-                                           partH_counts$GENDERDETAIL == 3]
-    }
-    if(sum(partH_counts$UGPB == 'GR' &
-           partH_counts$GENDERDETAIL == 4) == 1){
-      partH$EFGU022 <- partH_counts$COUNT[partH_counts$UGPB == 'GR' &
-                                           partH_counts$GENDERDETAIL == 4]
-    }
     #BUT -- New in 2023 - mask if < 5 and set initial inquiry as "small N"
     if(partH$EFGU022 < 5){
       partH$EFGU022 <- -2
       partH$EFGU02 <- 3
     }
-  }
 
   return(partH)
 
