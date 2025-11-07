a519563693b29f707e52f1df8d6d9b6ad01d3874
Alison Lanski <alanski@nd.edu>
2025-11-07 11:19:19 -0500

    applicant status counts should be duplicated across status options

--- R/make_adm_part_B.R
+++ R/make_adm_part_B.R
@@ -13,39 +13,64 @@ make_adm_part_B <- function(df) {
 
   colnames(df) <- stringr::str_to_upper(colnames(df))
 
-   ## pull first-time students for part B
-  partB <- df %>%
-    dplyr::filter(.data$ISFIRSTTIME == 1, .data$SEX != 3) %>%
+   ##first-time students for part B: all applicants/admits by RE & Sex
+  partB_AppAdm <- df %>%
+    dplyr::filter(.data$ISFIRSTTIME == 1) %>%
     dplyr::select("UNITID",
                   "SEX",
                   "RACEETHNICITY",
                   "ISAPPLICANT",
-                  "ISADMITTED",
-                  "ISENROLLED",
-                  "ISFULLTIME",
-                  "ISFIRSTTIME"
+                  "ISADMITTED"
     ) %>%
-    dplyr::mutate(LINE = dplyr::case_when(
-                                .data$ISADMITTED == 0 ~ 1,
-                                .data$ISENROLLED == 0 ~ 2,
-                                .data$ISENROLLED == 1 & .data$ISFULLTIME == 1 ~ 3,
-                                .data$ISENROLLED == 1 & .data$ISFULLTIME == 0 ~ 4
-    )) %>%
     dplyr::group_by(.data$UNITID,
-                    .data$LINE,
+                    .data$RACEETHNICITY,
+                    .data$SEX
+    ) %>%
+    dplyr::summarize(ApplyCount = dplyr::n(),
+                     AdmitCount = sum(.data$ISADMITTED, na.rm = TRUE)
+                     ) %>%
+    dplyr::ungroup() %>%
+    tidyr::pivot_longer(cols = c("ApplyCount", "AdmitCount"), names_to = 'Metric', values_to = 'COUNT') %>%
+    mutate(Line = ifelse(.data$Metric == 'ApplyCount', 1, 2))
+
+  # first time student for B: all enrolled by RE/Sex & Time Status
+  partB_Enrl <- df %>%
+    dplyr::filter(.data$ISFIRSTTIME == 1) %>%
+    dplyr::select("UNITID",
+                  "SEX",
+                  "RACEETHNICITY",
+                  "ISFULLTIME",
+                  "ISENROLLED"
+    ) %>%
+    dplyr::group_by(.data$UNITID,
                     .data$RACEETHNICITY,
                     .data$SEX,
+                    .data$ISFULLTIME
     ) %>%
-    dplyr::summarize(COUNT = n()) %>%
+    dplyr::summarize(EnrollCount = sum(.data$ISENROLLED, na.rm = TRUE)
+    ) %>%
     dplyr::ungroup() %>%
+    dplyr::mutate(Line = dplyr::case_when(ISFULLTIME == 1 ~ 3,
+                                          ISFULLTIME == 0 ~ 4,
+                                          TRUE ~ 99)) %>%
+    dplyr::select(-"ISFULLTIME") %>%
+    tidyr::pivot_longer(cols = c("EnrollCount"), names_to = 'Metric', values_to = 'COUNT')
+
+  # Combine all lines
+  partB <- dplyr::bind_rows(partB_AppAdm, partB_Enrl) %>%
+    # not necessary to remove 0s, but cleans it up
+    dplyr::filter(.data$COUNT > 0) %>%
+    # final form
     dplyr::transmute(UNITID = .data$UNITID,
                      SURVSECT = "ADM",
                      PART = "B",
-                     LINE = .data$LINE,
+                     LINE = .data$Line,
                      RACE = .data$RACEETHNICITY,
                      SEX = .data$SEX,
                      COUNT = .data$COUNT
-    )
+    ) %>%
+    dplyr::arrange(.data$LINE, .data$RACE, .data$SEX)
+
 
   return(partB)
 }
--- R/make_adm_part_F.R
+++ R/make_adm_part_F.R
@@ -11,42 +11,66 @@
 
 
 make_adm_part_F <- function(df) {
-# Pull transfer students for part F
 
   colnames(df) <- stringr::str_to_upper(colnames(df))
 
-  partF <- df %>%
-    dplyr::filter(.data$ISTRANSFER == 1,
-                  .data$SEX != 3) %>%
+  ## transfer students for part F: all applicants/admits by RE & Sex
+  partF_AppAdm <- df %>%
+    dplyr::filter(.data$ISTRANSFER == 1) %>%
     dplyr::select("UNITID",
                   "SEX",
                   "RACEETHNICITY",
                   "ISAPPLICANT",
-                  "ISADMITTED",
-                  "ISENROLLED",
-                  "ISFULLTIME",
-                  "ISTRANSFER") %>%
-    dplyr::mutate(LINE = dplyr::case_when(
-                                .data$ISADMITTED == 0 ~ 1,
-                                .data$ISENROLLED == 0 ~ 2,
-                                .data$ISENROLLED == 1 & .data$ISFULLTIME == 1 ~ 3,
-                                .data$ISENROLLED == 1 & .data$ISFULLTIME == 0 ~ 4
-    )) %>%
-    dplyr::group_by(.data$UNITID,
-                  .data$LINE,
-                  .data$RACEETHNICITY,
-                  .data$SEX,
+                  "ISADMITTED"
     ) %>%
-    dplyr::summarize(COUNT = n()) %>%
+    dplyr::group_by(.data$UNITID,
+                    .data$RACEETHNICITY,
+                    .data$SEX
+    ) %>%
+    dplyr::summarize(ApplyCount = dplyr::n(),
+                     AdmitCount = sum(.data$ISADMITTED, na.rm = TRUE)
+    ) %>%
     dplyr::ungroup() %>%
+    tidyr::pivot_longer(cols = c("ApplyCount", "AdmitCount"), names_to = 'Metric', values_to = 'COUNT') %>%
+    mutate(Line = ifelse(.data$Metric == 'ApplyCount', 1, 2))
+
+  # transfer student for F: all enrolled by RE/Sex & Time Status
+  partF_Enrl <- df %>%
+    dplyr::filter(.data$ISTRANSFER == 1) %>%
+    dplyr::select("UNITID",
+                  "SEX",
+                  "RACEETHNICITY",
+                  "ISFULLTIME",
+                  "ISENROLLED"
+    ) %>%
+    dplyr::group_by(.data$UNITID,
+                    .data$RACEETHNICITY,
+                    .data$SEX,
+                    .data$ISFULLTIME
+    ) %>%
+    dplyr::summarize(EnrollCount = sum(.data$ISENROLLED, na.rm = TRUE)
+    ) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(Line = dplyr::case_when(ISFULLTIME == 1 ~ 3,
+                                          ISFULLTIME == 0 ~ 4,
+                                          TRUE ~ 99)) %>%
+    dplyr::select(-"ISFULLTIME") %>%
+    tidyr::pivot_longer(cols = c("EnrollCount"), names_to = 'Metric', values_to = 'COUNT')
+
+  # Combine all lines
+  partF <- dplyr::bind_rows(partF_AppAdm, partF_Enrl) %>%
+    # not necessary to remove 0s, but cleans it up
+    dplyr::filter(.data$COUNT > 0) %>%
+    # final form
     dplyr::transmute(UNITID = .data$UNITID,
-                   SURVSECT = "ADM",
-                   PART = "F",
-                   LINE = .data$LINE,
-                   RACE = .data$RACEETHNICITY,
-                   SEX = .data$SEX,
-                   COUNT = .data$COUNT
-    )
+                     SURVSECT = "ADM",
+                     PART = "F",
+                     LINE = .data$Line,
+                     RACE = .data$RACEETHNICITY,
+                     SEX = .data$SEX,
+                     COUNT = .data$COUNT
+    ) %>%
+    dplyr::arrange(.data$LINE, .data$RACE, .data$SEX)
 
   return(partF)
 
